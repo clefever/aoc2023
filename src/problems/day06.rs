d46fd77589cc06577dff6bbb85c77a575517fba7
Chris Lefever <linuxcl4@gmail.com>
2023-12-06 01:25:21 -0500

    Refactoring for day 6

--- src/problems/day06.rs
+++ src/problems/day06.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 use crate::advent_of_code;
 
 #[allow(dead_code)]
@@ -8,33 +10,11 @@ pub fn run() {
 }
 
 fn part1(input: &[String]) -> i32 {
-    let mut product = 1;
-    let times = input[0].split(' ').filter(|&x| !x.is_empty()).skip(1).map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
-    let distances = input[1].split(' ').filter(|&x| !x.is_empty()).skip(1).map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
-    for i in 0..times.len() {
-        let mut wins = 0;
-        for time in 0..times[i] {
-            if (times[i] - time) * time > distances[i] {
-                wins += 1;
-            }
-        }
-
-        product *= wins;
-    }
-
-    product
+    let (times, distances) = input.iter().map(|line| line.split(' ').filter(|&x| !x.is_empty()).skip(1).map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>()).collect_tuple().unwrap();
+    (0..times.len()).map(|time| (0..times[time]).filter(|&t| (times[time] - t) * t > distances[time]).count() as i32).reduce(|a, b| a * b).unwrap()
 }
 
 fn part2(input: &[String]) -> i64 {
-    let time = input[0].split(' ').filter(|&x| !x.is_empty()).skip(1).collect::<Vec<_>>().join("").parse::<i64>().unwrap();
-    let distance = input[1].split(' ').filter(|&x| !x.is_empty()).skip(1).collect::<Vec<_>>().join("").parse::<i64>().unwrap();
-
-    let mut wins = 0;
-    for t in 0..time {
-        if (time - t) * t > distance {
-            wins += 1;
-        }
-    }
-
-    wins
+    let (time, distance) = input.iter().map(|line| line.split(' ').filter(|&x| !x.is_empty()).skip(1).collect::<Vec<_>>().join("").parse::<i64>().unwrap()).collect_tuple().unwrap();
+    (0..time).filter(|&t| (time - t) * t > distance).count() as i64
 }
